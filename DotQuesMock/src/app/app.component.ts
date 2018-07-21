e3455ac4f96b404a8e31b815c04c92fe649cad0e
Nagasarithavaddi <sarithanada93@gmail.com>
2018-07-22 00:35:56 +0530

    User Dashboard Changes

--- DotQuesMock/src/app/app.component.ts
+++ DotQuesMock/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,13 +10,31 @@ export class AppComponent {
   title = 'app';
 
   rows = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
+    { 
+      name: 'how to determine phone number line type',
+      desc:'This topic will describe you how to determine phone number line type' ,
+      totalViews:7
+    },
+    { 
+      name: 'Invalid object name Clients' ,
+      desc:'In this article, you will learn about solving the problem which is caused due to missing of clients table in SQL server.' ,
+      totalViews:5
+    },
+    { name: 'No templates matched the input template name is4ef',
+      desc:'when you work with identityserver4 you will create templates using commands...' ,
+      totalViews:15
+    },
+    { name: 'Fluent validations not working after adding in separate class library' ,
+      desc:'1This article will explain you how to make your fluent validation to work properly even after adding in separate class library.', 
+      totalViews:20
+    },
+    { name: 'could not parse the json file error at line number asp core appsettings',
+      desc:'This article will explain you how to resolve "could not parse the json file error at line number "' ,
+      totalViews:25
+    }
+   
   ];
   columns = [
-    { prop: 'name' },
-    { name: 'Gender' },
-    { name: 'Company' }
+    { prop: 'name' }
   ];
 }
--- DotQuesMock/src/app/app.module.ts
+++ DotQuesMock/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
+
 import { AppComponent } from './app.component';
 
 @NgModule({
